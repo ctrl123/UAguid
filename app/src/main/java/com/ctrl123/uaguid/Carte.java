277da44a1ac791e236c855cd3ff50bba3d6e177f
Knight972 <arg_jon@orange.fr>
2017-10-18 09:39:00 -0400

    id carte

--- app/src/main/java/com/ctrl123/uaguid/Carte.java
+++ app/src/main/java/com/ctrl123/uaguid/Carte.java
@@ -2,12 +2,17 @@ package com.ctrl123.uaguid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Carte extends AppCompatActivity {
+
+    public ImageView carte_ua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carte);
+
+        carte_ua = (ImageView) findViewById(R.id.imageView6);
     }
 }
